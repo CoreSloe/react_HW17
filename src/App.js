c575269a9104b5b14ea600cc37180ab5c37a39f1
CoreSloe <Coresloe92@gmail.com>
2023-08-08 11:20:35 +0300

    add homework17

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Contracts from "./components/Contracts";
+
+function App() {
+  return (
+    <div className="App">
+      <Contracts />
+    </div>
+  );
+}
+
+export default App;
